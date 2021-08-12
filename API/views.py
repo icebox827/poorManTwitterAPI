c4d688e20219af852a74ac1ab039757acfb08575
Denis Lafontant <icebox827@gmail.com>
2021-08-12 06:56:05 -0400

    Create view for tweet model

--- API/views.py
+++ API/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import tweet
+from . serializers import tweetSerializer
+
 # Create your views here.
+class tweetList(APIView) :
+
+    def get(self, request):
+    tweet1= tweet.objects.all()
+    serializer=tweetSerializer(tweet1, many=True)
+    return Response(serializer.data)
+
+    def post(self):
+        pass
