8bff26bd76205a718a8e73d6703ddbc2220f223f
Denis Lafontant <icebox827@gmail.com>
2021-08-12 06:19:46 -0400

    Create tweet model

--- API/models.py
+++ API/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class tweet(models.Model):
+    content = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=15)
