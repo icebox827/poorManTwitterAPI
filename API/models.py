500bab9ee4a26926e0de781a536bc5ddf39190b4
Denis Lafontant <icebox827@gmail.com>
2021-08-12 06:27:07 -0400

    Import and register tweet model to admin panel

--- API/models.py
+++ API/models.py
@@ -5,3 +5,6 @@ class tweet(models.Model):
     content = models.CharField(max_length=50)
     name = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
